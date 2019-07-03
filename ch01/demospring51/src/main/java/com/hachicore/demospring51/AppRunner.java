3978523c0a43cb29433bcc0766f3896a42f9086f
8c6t <nkw8620@gmail.com>
2019-07-03 17:50:45 +0900

    1-7. 프로퍼티
    -Dapp.name="hachicore"

--- ch01/demospring51/src/main/java/com/hachicore/demospring51/AppRunner.java
+++ ch01/demospring51/src/main/java/com/hachicore/demospring51/AppRunner.java
@@ -1,13 +1,12 @@
 package com.hachicore.demospring51;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
@@ -18,10 +17,14 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     BookRepository bookRepository;
 
+    @Value("${app.name}")
+    String appName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment env = ctx.getEnvironment();
-        System.out.println(Arrays.toString(env.getActiveProfiles()));
-        System.out.println(Arrays.toString(env.getDefaultProfiles()));
+        System.out.println(env.getProperty("app.name"));
+        System.out.println(env.getProperty("app.about"));
+        System.out.println(appName);
     }
 }
