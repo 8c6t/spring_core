b90a0f09de3bdd7f26d68b7b7589cc52528e2aef
8c6t <nkw8620@gmail.com>
2019-07-04 12:53:16 +0900

    1-10. ResourceLoader

--- ch01/demospring51/src/main/java/com/hachicore/demospring51/DemoApplication.java
+++ ch01/demospring51/src/main/java/com/hachicore/demospring51/DemoApplication.java
@@ -2,9 +2,6 @@ package com.hachicore.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
